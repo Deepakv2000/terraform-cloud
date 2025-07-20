f7f88dd804daaa0e235c870b6bd09a0793c5f3d7
Deepa <kgowdadeepa4@gmail.com>
2025-07-20 21:35:44 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "my-instance" {
+  ami = "ami-0f918f7e67a3323f0"
+  instance_type = "t2.micro"
+  key_name = "bata"
+  tags = {
+    Name = "demo"
+  }
+}
