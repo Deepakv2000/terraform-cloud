71f6b3096b5ad8d06115d598b047f61c5ee2b967
Deepa <kgowdadeepa4@gmail.com>
2025-07-20 22:01:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,12 @@ resource "aws_instance" "my-instance" {
     Name = "simha"
   }
 }
+
+resource "aws_instance" "my-instance2" {
+  ami = "ami-0f918f7e67a3323f0"
+  instance_type = "t2.micro"
+  key_name = "bata"
+  tags = {
+    Name = "simha1"
+  }
+}
